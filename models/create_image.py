099e6d32d1f8a5f22e83e85f9d36f6326845c041
AYUSH SHUKLA <shuklaayush00@gmail.com>
2024-11-06 15:47:48 +0530

    rm venv

--- models/create_image.py
+++ models/create_image.py
@@ -1,7 +1,7 @@
 import openai
 
 def generate_image(prompt):
-    openai.api_key = 'YOUR_OPENAI_API_KEY'
+    openai.api_key = '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'
     response = openai.Image.create(
         prompt=prompt,
         n=1,
